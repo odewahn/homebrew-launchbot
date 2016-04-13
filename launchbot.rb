c60cee0c87ce487f70321c5d1d9e9370c2673f11
Andrew Odewahn <andrew@odewahn.com>
2016-04-13 10:56:17 -0400

    updated sha in formula

--- launchbot.rb
+++ launchbot.rb
@@ -2,7 +2,7 @@ class Launchbot < Formula
   desc "Launch, discover, and share containers from your desktop."
   homepage "http://launchbot.io"
   url "https://github.com/odewahn/homebrew-launchbot/releases/download/0.0.6/launchbot-0.0.6.tar.gz"
-  sha256 "8c8a97c74e53bf45ec6bc443d1fa69cdd71d8aafd57d4394450159c990e9666a"
+  sha256 "7316e3510e24a3a4a119f0697718e50a2b095bb85d1dcfc48b90be110e4928d3"
 
   bottle :unneeded
 

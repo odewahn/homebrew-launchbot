1ef3dde9889632277e9781f8c94b77e08741c08e
Andrew Odewahn <andrew@odewahn.com>
2016-04-13 10:22:38 -0400

    embedded golang site

--- launchbot.rb
+++ launchbot.rb
@@ -1,8 +1,8 @@
 class Launchbot < Formula
   desc "Launch, discover, and share containers from your desktop."
   homepage "http://launchbot.io"
-  url "https://github.com/odewahn/homebrew-launchbot/releases/download/0.5.0/launchbot-0.5.0.tar.gz"
-  sha256 "3be350eddbd7a5c6a2ae097bedf1bd95f119c317ccf2a84910fd2b3976d416bd"
+  url "https://github.com/odewahn/homebrew-launchbot/releases/download/0.0.6/launchbot-0.0.6.tar.gz"
+  sha256 "8c8a97c74e53bf45ec6bc443d1fa69cdd71d8aafd57d4394450159c990e9666a"
 
   bottle :unneeded
 
